2f8e9e3f047b7ed3b9eb3467bd9f2193c60bcc7c
Dev-Kings <kingasiadavid41@gmail.com>
2023-10-17 05:43:00 -0700

    1 Task

---
+++ _printf_integer.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _printf_integer - prints integer
+ * @args: argument to print
+ *
+ * Return: number of characters printed
+ */
+int _printf_integer(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exp = 1, i = 1;
+
+	n = n / 10;
+	num = n;
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+	return (i);
+}
